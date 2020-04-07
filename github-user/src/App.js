a4bf74cf7952227caec1a205a87d172078e9716c
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-04-07 16:37:57 -0700

    some styles added

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,15 +1,37 @@
 
 import React from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardMedia from "@material-ui/core/CardMedia"
+import Typography from '@material-ui/core/Typography'
+import { red } from '@material-ui/core/colors';
+
 import './App.css';
  
+const useStyles = makeStyles({
+    root:{
+        maxWidth: 345,
+        backgroundColor: red[500]
+    },
+    media:{
+        height:0,
+        paddingTop: '56.24%',
+    },
+})
+
+
+
 
 class App extends React.Component{
-    state = {
-        users: [],
-        userText: ''
+    constructor(){
+    super()
+    this.state = {
+        users: []
     }
+}
 
+   
 
     componentDidMount() {
         axios
@@ -24,24 +46,28 @@ class App extends React.Component{
             })
             .catch(err => console.log(err.message))
     }
+
+    
+
     
 
     render(){
         return(
             <div className = "App">
                 <h1>Github Friends</h1>
-                <div className = "git-friends">
+                <Card className = "git-friends">
                     {this.state.users.map(user => {
                         console.log(user)
                     return (
                         <div>
-                            <img width="200" src={user.avatar_url} key={user.id} alt={user}/>
-                    <p>Username: {user.login}</p>
-                    <p>followers: {user.followers_url.length}</p>
+
+                            <img  width="200" src={user.avatar_url} key={user.id} alt={user}/>
+                    <Typography >Username: {user.login}</Typography>
+                    <Typography>followers: {user.followers_url.length}</Typography>
                         </div>
                     )})}
 
-                </div>
+                </Card>
             </div>
         )
     }
